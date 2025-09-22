290453e03f36c61b2d42c69198e6d5d5831c3fd3
Sinaya Naik <sinayanaik@gmail.com>
2025-09-22 17:54:05 +0530

    removed unnecessary packages

--- arm_motion/launch/motion.launch.py
+++
@@ -1,9 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='arm_motion', executable='motion_node.py', output='screen'
-        )
-    ])
